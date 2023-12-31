2cd51966f076e9067fa4473f08cf19029844e471
devkramit <dev.amit.kumar@hotmail.com>
2024-01-01 01:14:09 +0530

    Data types summary

--- 01_Basics/05_datatypes_summary.js
+++ 01_Basics/05_datatypes_summary.js
@@ -1,17 +1,34 @@
 /**
- * Prints a greeting message.
- * @param {string} name - The name to be greeted.
+ * This file provides an overview of different data types in JavaScript.
  */
+
 console.log("Hey! Amit");
 
 /**
- * Adds a number to itself.
+ * Adds a given number to itself.
  * @param {number} num - The number to be added.
- * @returns {number} The sum of the number and itself.
+ * @returns {number} - The sum of the given number and itself.
  */
-
 function toAdd(num) {
     return num + num;
 }
 
 console.log(toAdd(10));
+
+// Primitive types - String, Number, boolean, Null, undefined
+
+// Reference type (Non- Primitive)
+
+// Array, Objects, Function
+
+let myArr = ["amit", "kumar", "engineer"]; // example of Array
+let myObj = { // Example of an object
+    name : "Amit",
+    surname : "Kumar",
+    designation : "Engineer"
+}
+let myFunction = function(){ // example of a function
+    console.log("This is my function!");
+}
+
+console.table([myArr, myObj]);
